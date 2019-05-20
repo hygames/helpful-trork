da7aafa1797fe9a3d9c99a9af4df7ae89a8d5063
Rafael <rafael.hoefener@gmail.com>
2019-05-20 10:59:28 +0200

    added Ping resoponse unfinished

---
+++ src/main/java/co/hygames/helpfultrork/Ping.java
@@ -0,0 +1,14 @@
+package co.hygames.helpfultrork;
+
+public class Ping {
+    /*Pong response for ping
+     */
+   if (message("ping") == true)
+    {
+        System.out.println("Pong!");
+    }
+   else
+    {
+        System.out.println("I cant help you with that. Let me sleep!");
+    }
+}
